8cfb4b29c7b3d174fca2fbb7ed21dd527435a0d2
Vlad Korneev <veenrok@veenrok.com>
2026-03-11 17:49:00 +0300

    feat: add  get_cheaper_alternative method

--- src/app/repository.py
+++ src/app/repository.py
@@ -7,6 +7,9 @@ class BaseProductRepository:
     def get_products_by_region(self, region: str) -> Sequence[Product]:
         raise NotImplementedError
 
+    def get_cheaper_alternative(self, product: Product) -> Product | None:
+        raise NotImplementedError
+
     def get_available_region_names(self) -> Sequence[str]:
         raise NotImplementedError
 
@@ -19,6 +22,10 @@ class InMemoryProductRepository(BaseProductRepository):
 
     def get_products_by_region(self, region: str) -> Sequence[Product]:
         return [p for p in self._products if p.region == region]
+
+    def get_cheaper_alternative(self, product: Product) -> Product | None:
+        filtered = filter(lambda p: p.region == product.region and p.category == product.category and p.price < product.price, self._products)
+        return next(filtered, None,)
 
     def get_available_region_names(self) -> Sequence[str]:
         return sorted(set(p.region for p in self._products))
