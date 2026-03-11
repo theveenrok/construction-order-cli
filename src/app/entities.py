d6912bc9d6e152a7de282c7f097baef7984b63ef
Vlad Korneev <veenrok@veenrok.com>
2026-03-11 17:16:00 +0300

    feat: add basic entities.
    - add product entity.
    - add comercial offer entity.
    - add order entity.

---
+++ src/app/entities.py
@@ -0,0 +1,53 @@
+from __future__ import annotations
+
+import dataclasses
+
+
+@dataclasses.dataclass
+class Product:
+    """Construction product.
+
+    Attributes:
+
+        name: Product name.
+        price: Product price in rubles.
+        category: Product category.
+        region: Region of sale.
+    """
+
+    name: str
+    price: float
+    category: str
+    region: str
+
+
+@dataclasses.dataclass
+class CommercialOffer:
+    """Comercial offer for a product.
+    Used when forming an order.
+
+    Attributes:
+
+         product: Product for which the offer is made.
+         total_price: Total price of the offer in rubles."""
+
+    product: Product
+    total_price: float
+
+
+@dataclasses.dataclass
+class Order:
+    """Order for a product.
+
+    Attributes:
+
+        region: Region of sale.
+        product_name: Product name.
+        product_category: Product category.
+        total_price: Total price of the order in rubles.
+    """
+
+    region: str
+    product_name: str
+    product_category: str
+    total_price: float
