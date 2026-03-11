35fc21e52093ccd9f7c9456d16955e6fa14ddae6
Vlad Korneev <veenrok@veenrok.com>
2026-03-11 17:48:39 +0300

    feat: remove get_products_by_region_and_category method

--- src/app/repository.py
+++ src/app/repository.py
@@ -7,9 +7,6 @@ class BaseProductRepository:
     def get_products_by_region(self, region: str) -> Sequence[Product]:
         raise NotImplementedError
 
-    def get_products_by_region_and_category(self, region: str, category: str) -> Sequence[Product]:
-        raise NotImplementedError
-
     def get_available_region_names(self) -> Sequence[str]:
         raise NotImplementedError
 
@@ -22,9 +19,6 @@ class InMemoryProductRepository(BaseProductRepository):
 
     def get_products_by_region(self, region: str) -> Sequence[Product]:
         return [p for p in self._products if p.region == region]
-
-    def get_products_by_region_and_category(self, region: str, category: str) -> Sequence[Product]:
-        return [p for p in self._products if p.region == region and p.category == category]
 
     def get_available_region_names(self) -> Sequence[str]:
         return sorted(set(p.region for p in self._products))
